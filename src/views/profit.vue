2e18a4dbee29bd7b97304c9336b16421bd1e0f23
奋斗2014 <1083481902@qq.com>
2022-02-23 21:55:52 +0800

    1

--- src/views/profit.vue
+++ src/views/profit.vue
@@ -195,9 +195,9 @@ export default {
     const showScene = (status) => {
       switch (status) {
         case 1:
-          return "挖矿";
+          return "量化交易收入";
         case 2:
-          return "挖矿返利";
+          return "量化交易返利";
         case 3:
           return "充值";
         case 4:
@@ -206,6 +206,10 @@ export default {
           return "邀请返利";
         case 6:
           return "注册礼金";
+        case 7:
+          return "提现";
+        case 8:
+          return "转到基础账户";
       }
       return status;
     };
